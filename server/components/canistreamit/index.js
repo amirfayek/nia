74536163a9e650b9605f75a6421248d43ad896ff
randallkanna <randallkanna@gmail.com>
2015-01-06 18:15:22 -0800

    MERGE THIS RIGHT AWAY - URGENT design fixes

--- server/components/canistreamit/index.js
+++ server/components/canistreamit/index.js
@@ -1,8 +1,8 @@
-var http = require('http');
-var request = require('request');
+// var http = require('http');
+// var request = require('request');
 
-module.exports = function(options) {
-  function search(movieName) {
+// module.exports = function(options) {
+//   function search(movieName) {
 
-  }
-}
+//   }
+// }
