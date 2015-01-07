94d5889909aa3d911fa86fb9ffd142ec394c1474
Amir Fayek <afayek88@gmail.com>
2015-01-06 20:04:40 -0800

    added current master from github

--- client/app/movieshow/movieshow.controller.js
+++ client/app/movieshow/movieshow.controller.js
@@ -1,6 +1,29 @@
 'use strict';
 
 angular.module('niaApp')
-  .controller('MovieshowCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('MovieshowCtrl', function ($scope, $http, $routeParams) {
+    // var movieName = $routeParams.movieName;
+    // console.log(movieName);
+    $http.get('http://localhost:9000/api/movies/batman').
+      success(function(data, status, headers, config) {
+        console.log(data)
+        $scope.movies = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log("Unexpected error!")
+      })
   });
+
+
+
+// angular.module('niaApp')
+//   .controller("MovieshowCtrl", function ($scope, $http) {
+//       $http.get("../server/api/movie/movie.controller.js").success(function (data) {
+//         $scope.movie = data;
+//         console.log(data);
+
+//       }).error(function() {
+//         alert("HELP");
+//       });
+//   });
+
