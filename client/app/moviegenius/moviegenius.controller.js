5c6f26cce633ef1062daed7499ed2240810dade5
randallkanna <randallkanna@gmail.com>
2015-01-06 20:11:33 -0800

    changed controller and added a movie genius feature

--- client/app/moviegenius/moviegenius.controller.js
+++ client/app/moviegenius/moviegenius.controller.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('niaApp')
-  .controller('MoviegeniusCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('MoviegeniusCtrl', function ($scope, $http, $routeParams) {
+
+    var randomMovie = Math.random(1, 300);
+    console.log(randomMovie);
+
+    $http.get('http://localhost:9000/api/movies/batman').
+      success(function(data, status, headers, config) {
+        console.log(data)
+        $scope.movies = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log("Unexpected error!")
+      })
   });
+
+
+
+
