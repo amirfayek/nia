12ed1f953da8a5ac5e3ab0d1c4192745a12ef293
randallkanna <randallkanna@gmail.com>
2015-01-07 16:09:22 -0800

    yay

--- client/app/moviegenius/moviegenius.controller.js
+++ client/app/moviegenius/moviegenius.controller.js
@@ -1,12 +1,34 @@
+// 'use strict';
+
+// angular.module('niaApp')
+//   .controller('MoviegeniusCtrl', function ($scope, $http) {
+//     var movieList = ["Vertigo", "Batman Begins", "Battle Royale", "The Simpsons", "The Shining"];
+//     var randomMovie = movieList.sample();
+//     var uri = "http://localhost:9000/api/movies/" + randomMovie;
+//     $http.get(uri).
+//       success(function(data, status, headers, config) {
+//         $scope.movies = data;
+//       }).
+//       error(function(data, status, headers, config) {
+//         console.log("Unexpected error!");
+//       });
+//   });
+
+
 'use strict';
 
 angular.module('niaApp')
   .controller('MoviegeniusCtrl', function ($scope, $http, $routeParams) {
 
-    var randomMovie = Math.random(1, 300);
+    var movieList = ["Vertigo", "Batman Begins", "Battle Royale", "The Simpsons", "The Shining", "A Beautiful Mind", "Harry Potter"];
+    function getInt(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    };
+    var randomNum = getInt(0, movieList.length);
+    var randomMovie = movieList[randomNum];
     console.log(randomMovie);
 
-    $http.get('http://localhost:9000/api/movies/batman').
+    $http.get('http://localhost:9000/api/movies/' + randomMovie).
       success(function(data, status, headers, config) {
         console.log(data)
         $scope.movies = data;
@@ -15,7 +37,3 @@ angular.module('niaApp')
         console.log("Unexpected error!")
       })
   });
-
-
-
-
--- client/app/movieshow/movieshow.controller.js
+++ client/app/movieshow/movieshow.controller.js
@@ -2,20 +2,11 @@
 
 angular.module('niaApp')
   .controller('MovieshowCtrl', function ($scope, $http, $routeParams) {
-    // var movieName = $routeParams.movieName;
-    // console.log(movieName);
-    // console.log($routeParams);
     var movieName = $routeParams.moviename.toLowerCase();
-    // console.log(movieName);
     var uri = "http://localhost:9000/api/movies/" + movieName;
-
-    // console.log("this is the uri:")
-    // console.log(uri);
-
 
     $http.get(uri).
       success(function(data, status, headers, config) {
-        // console.log(data)
         $scope.movies = data;
       }).
       error(function(data, status, headers, config) {
