3dd750a3245a03c0049bc846e9a5ad9755930faa
randallkanna <randallkanna@gmail.com>
2015-01-08 10:21:49 -0800

    SO MANY CHANGES

--- client/app/movieshow/movieshow.controller.js
+++ client/app/movieshow/movieshow.controller.js
@@ -2,34 +2,15 @@
 
 angular.module('niaApp')
   .controller('MovieshowCtrl', function ($scope, $http, $routeParams) {
-    // var movieName = $routeParams.movieName;
-    // console.log(movieName);
-    // console.log($routeParams);
     var movieName = $routeParams.moviename.toLowerCase();
-    // console.log(movieName);
     var uri = 'http://localhost:9000/api/movies/' + movieName;
-
 
     $http.get(uri).
       success(function(data, status, headers, config) {
         console.log(data)
-        $scope.movies = data;
+        $scope.movies = [data];
       }).
       error(function(data, status, headers, config) {
         console.log("Unexpected error!")
       })
   });
-
-
-
-// angular.module('niaApp')
-//   .controller("MovieshowCtrl", function ($scope, $http) {
-//       $http.get("../server/api/movie/movie.controller.js").success(function (data) {
-//         $scope.movie = data;
-//         console.log(data);
-
-//       }).error(function() {
-//         alert("HELP");
-//       });
-//   });
-
