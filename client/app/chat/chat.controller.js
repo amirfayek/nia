66b3be01e02a7e25c476f33842286d57c9ebfbbe
Amir Fayek <afayek88@gmail.com>
2015-05-02 15:12:53 -0700

    jshints tests passing

--- client/app/chat/chat.controller.js
+++ client/app/chat/chat.controller.js
@@ -4,19 +4,18 @@ angular.module('niaApp')
   .controller('ChatCtrl', function ($scope, Auth, ChatService) {
   	
   	$scope.users = ChatService.users;
-
-  	console.log("*ChatService`users*", ChatService.users)
+  	// console.log('*ChatService`users*', ChatService.users);
     $scope.messages = ChatService.messages;
     $scope.user = Auth.getCurrentUser();
-    $scope.newMessage = "";
+    $scope.newMessage = '';
   	
-
     $scope.say = function() {
     	ChatService.say($scope.newMessage);
-    	$scope.newMessage = "";
+    	$scope.newMessage = '';
     };
+
     $scope.loadUsers = function(){
-    	console.log("*$scope.loadUsers*");
+    	console.log('*$scope.loadUsers*');
     	ChatService.loadUsers();
     };
 
