89e3d58a1b1085073460de9530334f0474cd5b86
Amir Fayek <afayek88@gmail.com>
2015-06-16 20:22:23 -0700

    refactor

--- server/components/rottentomatoes/index.js
+++ server/components/rottentomatoes/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var request = require('request');
-var http = require('http');
-var Q = require('q');
+var request = require('request'),
+    http = require('http'),
+    Q = require('q');
 
-var module = module.exports = {
+module.exports = {
 
   topMovies: function() {
-    var movieArray = new Array();
+    var movieArray = [];
     request('http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=n98uq7kqyp3xc9hw3tq6hn6r', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
+      if (!error && response.statusCode === 200) {
         var movie = JSON.parse(body)
         for (var key in movie) {
           movieArray.push(movie[key])
@@ -22,7 +22,7 @@ var module = module.exports = {
 
   currentReleases: function() {
     request('http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/current_releases.json?apikey=n98uq7kqyp3xc9hw3tq6hn6r', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
+      if (!error && response.statusCode === 200) {
         var movie = JSON.parse(body)
         // for (var key in movie) {
           // console.log(movie[key])
@@ -71,5 +71,3 @@ var module = module.exports = {
 
 
 };
-
-module.topMovies();
