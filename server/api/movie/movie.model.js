9ba0087bc6abba6d406f8a849e28847b26b8b2ef
Amir Fayek <afayek88@gmail.com>
2015-06-25 19:21:26 -0700

    update movie model

--- server/api/movie/movie.model.js
+++ server/api/movie/movie.model.js
@@ -9,12 +9,32 @@ var MovieSchema = new Schema({
   updated: { type: Date, default: Date.now},
   title: String,
   year: Number,
-  desc: String,
-  img: String,
-  actors: [{ name: String }],
+  description: String,
+  imgage: String,
+  cast: [{ name: String }],
   ratings: Number,
   active: Boolean,
   date: [{ type: Date, default: Date.now}],
+  providers: {
+    rottenTomatoes: {
+      ratings: {
+        critics: { type: Number }, 
+        audience: { type: Number }, 
+      },
+    },
+    imdb: { 
+      url: { type: String }
+    },
+    canIStreamIt: { 
+      id: { type: String }
+    },
+    metaCritic: { type: String},
+    netflix: {
+      id: { type: String },
+      url: { type: String },
+      price: { type: Number }
+    }
+  },
   meta: {
     favorites: [ {type : mongoose.Schema.ObjectId, ref : 'User'} ]
   }
