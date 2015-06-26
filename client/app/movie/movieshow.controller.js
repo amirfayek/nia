9f4327a853a2db3f060826d85a93725a7ed57534
Amir Fayek <afayek88@gmail.com>
2015-06-25 19:17:50 -0700

    update movie info to reflect server change. fix links

--- client/app/movie/movieshow.controller.js
+++ client/app/movie/movieshow.controller.js
@@ -1,22 +1,23 @@
 'use strict';
 
-angular.module('niaApp').controller('MovieShowCtrl', function ($scope, $http, $routeParams, User) {
+angular.module('niaApp')
+.controller('MovieShowCtrl', function ($scope, $http, $routeParams, Auth, User) {
   var movieName = $routeParams.id.toLowerCase();
   var uri = '/api/movies/' + movieName;
+  var currentUser = Auth.getCurrentUser();
 
   $http.get(uri).
-    success (function(data, status, headers, config) {
-      console.log(data);
+    success(function(data, status, headers, config) {
       $scope.movies = [data];
+      console.log(data);
     }).
-    error (function(data, status, headers, config) {
+    error(function(data, status, headers, config) {
       console.log("Unexpected error!");
     });
 
-  $scope.addFavorite = function(movie) {
+  $scope.toggleFavorite = function(movie) {
     console.log("add");
-    console.log(req.user._id);
-    // movie.favorites.users.push(User);
+    $http.post('/api/users/' + currentUser._id + '/favorites', { movie: $scope.movies });
   }
 
 });
